0fe1afc51d14d8fc9df810fde0f35755195dd0af
Andres Vargas <vargasa@cua.edu>
2022-03-21 22:48:16 +0100

    Set HLT35 Pho175 Triggers

--- etc/inputs/tnpSampleDef.py
+++ etc/inputs/tnpSampleDef.py
@@ -15,9 +15,11 @@ eosUL2017 = '/eos/cms/store/group/phys_egamma/asroy/Tag-and-Probe_Tree/UL2017_MI
 eosUL2018 = '/eos/cms/store/group/phys_egamma/asroy/Tag-and-Probe_Tree/UL2018_MINIAOD_Nm1/'
 eosUL2016 = '/eos/cms/store/group/phys_egamma/akapoor/Tag-and-Probe_Tree/UL2016_ntuples/'
 
+customUL = '/eos/cms/store/user/avargash/EGammaSF/2022-03-16/'
 
 
 ReReco2017 = {
+
 
     'DY_madgraph'              : tnpSample('DY_madgraph',
                                        eosReReco2017 + 'DYJetsToLL.root',
@@ -84,72 +86,40 @@ ReReco2018 = {
 
 
 UL2017 = {
-    ### MiniAOD TnP for IDs scale factors
-    'DY_madgraph'              : tnpSample('DY_madgraph',
-                                       eosUL2017 + 'DYJetsToEE.root ',
-                                       isMC = True, nEvts =  -1 ),
-#    'DY_amcatnlo'                 : tnpSample('DY_amcatnlo',
-#                                       eosUL2017 + 'DYJetsToLLM50amcatnloFXFX.root',
-#                                       isMC = True, nEvts =  -1 ),
-    'DY_amcatnloext'                 : tnpSample('DY_amcatnloext',
-                                       eosUL2017 + 'DYJetsToLL_amcatnloFXFX.root',
-                                       isMC = True, nEvts =  -1 ),
-
-
-    'data_Run2017B' : tnpSample('data_Run2017B' , eosUL2017 + 'SingleEle_RunB.root' , lumi = 4.793961427),
-    'data_Run2017C' : tnpSample('data_Run2017C' , eosUL2017 + 'SingleEle_RunC.root' , lumi = 9.631214821 ),
-    'data_Run2017D' : tnpSample('data_Run2017D' , eosUL2017 + 'SingleEle_RunD.root' , lumi = 4.247682053 ),
-    'data_Run2017E' : tnpSample('data_Run2017E' , eosUL2017 + 'SingleEle_RunE.root' , lumi = 9.313642402 ),
-    'data_Run2017F' : tnpSample('data_Run2017F' , eosUL2017 + 'SingleEle_RunF.root' , lumi = 13.510934811),
-
+    'DY_NLO' : tnpSample('DY_NLO', customUL + 'UL2017_DYToLL_NLO.root ', isMC = True, nEvts =  -1 ),
+    'DY_LO' : tnpSample('DY_NLO', customUL + 'UL2017_DYToLL_LO.root', isMC = True, nEvts =  -1 ),
+    'data_Run2017B' : tnpSample('data_Run2017B' , customUL + 'SingleEle_RunB.root' , lumi = 4.793961427),
+    'data_Run2017C' : tnpSample('data_Run2017C' , customUL + 'SingleEle_RunC.root' , lumi = 9.631214821 ),
+    'data_Run2017D' : tnpSample('data_Run2017D' , customUL + 'SingleEle_RunD.root' , lumi = 4.247682053 ),
+    'data_Run2017E' : tnpSample('data_Run2017E' , customUL + 'SingleEle_RunE.root' , lumi = 9.313642402 ),
+    'data_Run2017F' : tnpSample('data_Run2017F' , customUL + 'SingleEle_RunF.root' , lumi = 13.510934811),
     }
 
 UL2018 = {
-    ### MiniAOD TnP for IDs scale factors
-    'DY_madgraph'              : tnpSample('DY_madgraph',
-                                       eosUL2018 + 'DYJetsToLL_madgraphMLM.root',
-                                       isMC = True, nEvts =  -1 ),
-    'DY_amcatnloext'                 : tnpSample('DY_amcatnloext',
-                                       eosUL2018 + 'DYJetsToLL_amcatnloFXFX.root',
-                                       isMC = True, nEvts =  -1 ),
-
-
-    'data_Run2018A' : tnpSample('data_Run2018A' , eosUL2018 + 'EGamma_RunA.root' , lumi = 14.02672485),
-    'data_Run2018B' : tnpSample('data_Run2018B' , eosUL2018 + 'EGamma_RunB.root' , lumi = 7.060617355),
-    'data_Run2018C' : tnpSample('data_Run2018C' , eosUL2018 + 'EGamma_RunC.root' , lumi = 6.894770971),
-    'data_Run2018D' : tnpSample('data_Run2018D' , eosUL2018 + 'EGamma_RunD.root' , lumi = 31.74220577),
+    'DYToLL_NLO'   : tnpSample('DYToLL_NLO', customUL + 'UL2018_DYToLL_NLO.root', isMC = True, nEvts =  -1 ),
+    'DYToLL_LO': tnpSample('DYToLL_LO', customUL + 'UL2018_DYToLL_LO.root', isMC = True, nEvts =  -1 ),
+    'data_Run2018A' : tnpSample('data_Run2018A' , customUL + 'UL2018_EGRun2018A.root' , lumi = 14.02672485),
+    'data_Run2018B' : tnpSample('data_Run2018B' , customUL + 'UL2018_EGRun2018B.root' , lumi = 7.060617355),
+    'data_Run2018C' : tnpSample('data_Run2018C' , customUL + 'UL2018_EGRun2018C.root' , lumi = 6.894770971),
+    'data_Run2018D' : tnpSample('data_Run2018D' , customUL + 'UL2018_EGRun2018D.root' , lumi = 31.74220577),
     }
 
 
 UL2016_preVFP = {
-    ### MiniAOD TnP for IDs scale factors
-    'DY_madgraph'              : tnpSample('DY_madgraph',
-                                       eosUL2016 + 'DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8_preVFP_UL2016.root',
-                                       isMC = True, nEvts =  -1 ),
-    'DY_amcatnloext'                 : tnpSample('DY_amcatnloext',
-                                       eosUL2016 + 'DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8_preVFP_UL2016.root',
-                                       isMC = True, nEvts =  -1 ),
-
-
-    'data_Run2016B' : tnpSample('data_Run2016B' , eosUL2016 + 'UL2016_SingleEle_Run2016B.root' , lumi = 0.030493962),
-    'data_Run2016B_ver2' : tnpSample('data_Run2016B_ver2' , eosUL2016 + 'UL2016_SingleEle_Run2016B_ver2.root' , lumi = 5.879330594),
-    'data_Run2016C' : tnpSample('data_Run2016C' , eosUL2016 + 'UL2016_SingleEle_Run2016C.root' , lumi = 2.64992914),
-    'data_Run2016D' : tnpSample('data_Run2016D' , eosUL2016 + 'UL2016_SingleEle_Run2016D.root' , lumi = 4.292865604),
-    'data_Run2016E' : tnpSample('data_Run2016E' , eosUL2016 + 'UL2016_SingleEle_Run2016E.root' , lumi = 4.185165152),
-    'data_Run2016F' : tnpSample('data_Run2016F' , eosUL2016 + 'UL2016_SingleEle_Run2016F.root' , lumi = 2.725508364),
+    'DYToLL_NLO' : tnpSample('DYToLL_NLO', customUL + 'UL2016preVFP_DYToLL_NLO.root',isMC = True, nEvts =  -1 ),
+    'DYToLL_LO'  : tnpSample('DYToLL_LO', customUL + 'UL2016preVFP_DYToLL_LO.root',isMC = True, nEvts =  -1 ),
+    'data_Run2016B' : tnpSample('data_Run2016B' , customUL + 'UL2016_SingleEle_Run2016B.root' , lumi = 0.030493962),
+    'data_Run2016B_ver2' : tnpSample('data_Run2016B_ver2' , customUL + 'UL2016_SingleEle_Run2016B_ver2.root' , lumi = 5.879330594),
+    'data_Run2016C' : tnpSample('data_Run2016C' , customUL + 'UL2016_SingleEle_Run2016C.root' , lumi = 2.64992914),
+    'data_Run2016D' : tnpSample('data_Run2016D' , customUL + 'UL2016_SingleEle_Run2016D.root' , lumi = 4.292865604),
+    'data_Run2016E' : tnpSample('data_Run2016E' , customUL + 'UL2016_SingleEle_Run2016E.root' , lumi = 4.185165152),
+    'data_Run2016F' : tnpSample('data_Run2016F' , customUL + 'UL2016_SingleEle_Run2016F.root' , lumi = 2.725508364),
     }
 
 UL2016_postVFP = {
-    ### MiniAOD TnP for IDs scale factors
-    'DY_madgraph'              : tnpSample('DY_madgraph',
-                                       eosUL2016 + 'DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8_postVFP_UL2016.root',
-                                       isMC = True, nEvts =  -1 ),
-    'DY_amcatnloext'                 : tnpSample('DY_amcatnloext',
-                                       eosUL2016 + 'DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8_postVFP_UL2016.root',
-                                       isMC = True, nEvts =  -1 ),
-
-
-    'data_Run2016F_postVFP' : tnpSample('data_Run2016F_postVFP' , eosUL2016 + 'UL2016_SingleEle_Run2016F_postVFP.root' , lumi = 0.414987426),
-    'data_Run2016G' : tnpSample('data_Run2016G' , eosUL2016 + 'UL2016_SingleEle_Run2016G.root' , lumi = 7.634508755),
-    'data_Run2016H' : tnpSample('data_Run2016H' , eosUL2016 + 'UL2016_SingleEle_Run2016H.root' , lumi = 8.802242522),
+    'DYToLL_NLO' : tnpSample('DYToLL_NLO', customUL + 'UL2016postVFP_DYToLL_NLO.root', isMC = True, nEvts =  -1 ),
+    'DYToLL_LO' : tnpSample('DYToLL_LO', customUL + 'UL2016postVFP_DYToLL_LO.root', isMC = True, nEvts =  -1 ),
+    'data_Run2016F_postVFP' : tnpSample('data_Run2016F_postVFP' , customUL + 'UL2016_SingleEle_Run2016F_postVFP.root' , lumi = 0.414987426),
+    'data_Run2016G' : tnpSample('data_Run2016G' , customUL + 'UL2016_SingleEle_Run2016G.root' , lumi = 7.634508755),
+    'data_Run2016H' : tnpSample('data_Run2016H' , customUL + 'UL2016_SingleEle_Run2016H.root' , lumi = 8.802242522),
     }
